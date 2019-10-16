b970fa7481e5ab8a558dba71b441a0a47f2edb59
Anne Fausto <a.r.fausto@gmail.com>
2019-10-16 01:30:19 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,18 @@
 class Appointment 
   
+  @@all = []
+  
   attr_accessor :date, :patient, :doctor
   
-  def initialize(date,
+  def initialize(date,patient, doctor)
+    @date = date
+    @patient = patient 
+    @doctor = doctor
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+end 
