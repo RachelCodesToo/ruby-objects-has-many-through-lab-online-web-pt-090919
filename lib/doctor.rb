829a85be71721c45474c43c2fdc9f3d162fc1aa5
Anne Fausto <a.r.fausto@gmail.com>
2019-10-16 02:13:21 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,10 +19,6 @@ class Doctor
   
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
-  end 
-  
-  def patients
-    appointments.map{|appointment| appointment.patient}
-  end 
+  end
   
 end 
